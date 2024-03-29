37ebadff2d1586a610461a5a0879c94eabb89176
trevorxarcieri <agent.steel.ta@gmail.com>
2024-03-29 13:39:13 -0400

    updated deletion script, fixed bugs in fill perms

--- delete_csv_perms.sh
+++ delete_csv_perms.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-gam csvfile $1_file_ids_and_permIds.csv:Owner delete permissions csvfile $1_file_ids_and_permIds.csv:id csvfile $1_file_ids_and_permIds.csv:permId
+# Check if the first argument is empty. If so, set it to "all"
+if [ -z "$1" ]; then
+  filename="all"
+else
+  filename="$1"
+fi
+
+# Use GNU Parallel to process each line in parallel
+parallel -d "\r\n" --skip-first-line --colsep ',' gam user {1} delete permission id {2} {3} :::: "${filename}_file_ids_and_permIds.csv"
+
